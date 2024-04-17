8af7bb03a926a5d2d22ad6ba633d41dd9b2185f4
stencildev <100499903+stencildev@users.noreply.github.com>
2024-04-16 20:05:50 -0600

    updated apt install line

--- install-docker-debian.sh
+++ install-docker-debian.sh
@@ -2,7 +2,7 @@
 
 # Add Docker's official GPG key:
  apt update
- apt install ca-certificates curl
+ apt install -y ca-certificates curl
  install -m 0755 -d /etc/apt/keyrings
  curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
  chmod a+r /etc/apt/keyrings/docker.asc
@@ -12,6 +12,6 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
    tee /etc/apt/sources.list.d/docker.list > /dev/null
- apt-get update
+ apt update
 
- apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+ apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
