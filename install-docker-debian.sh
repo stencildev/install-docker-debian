b06ffd674d8c0c8d36650e4b120ce0889d939763
stencildev <100499903+stencildev@users.noreply.github.com>
2024-04-16 20:19:02 -0600

    Added Portainer installation to script

--- install-docker-debian.sh
+++ install-docker-debian.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Docker
+
 # Add Docker's official GPG key:
  apt update
  apt install -y ca-certificates curl
@@ -13,5 +15,14 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
    tee /etc/apt/sources.list.d/docker.list > /dev/null
  apt update
-
+# Install Docker
  apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Portainer
+
+# Create a Portainer Volume
+docker volume create portainer_data
+
+# Install and run Portainer using v2.19.4
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.19.4
+
